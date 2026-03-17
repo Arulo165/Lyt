d54e81864ff34b5842a02694a681566bdc7e52fa
Arulo165 <arulomodding@gmail.com>
2026-03-17 10:22:53 +0100

    add ResourceAccessor.h

---
+++ include/nw/lyt/lyt_ResourceAccessor.h
@@ -0,0 +1,15 @@
+#ifndef NW_LYT_RESOURCE_ACCESSOR_H
+#define NW_LYT_RESOURCE_ACCESSOR_H
+
+namespace nw { namespace lyt {
+
+    class ResourceAccessor
+    {
+    public:
+        ResourceAccessor();
+        virtual ~ResourceAccessor();
+    };
+
+} } // namespace nw::lyt
+
+#endif // NW_LYT_RESOURCE_ACCESSOR_H
